69b222bd89a7a1c1d077873a1f8ad88413d4a8bd
Sergey Savelyev <sergeysav.nn@gmail.com>
2017-10-06 16:43:26 -0400

    adds a testing rest api

--- src/main/kotlin/com/sergey/web/TestRestController.kt
+++ src/main/kotlin/com/sergey/web/TestRestController.kt
@@ -1,2 +1,13 @@
 package com.sergey.web
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestRestController {
+    @GetMapping("/testrest")
+    fun test(@RequestParam(value = "d1", required = false, defaultValue = "0") data1: Int,
+             @RequestParam(value = "d2", required = false, defaultValue = "") data2: String)
+            = TestObject(data1, data2)
+}
